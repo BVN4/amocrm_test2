33fa420c3d013a76986aa92ab3a21f6784fd4a39
ryanborisov <ryanborisov@team.amocrm.com>
2022-08-24 16:18:43 +0200

    Add composer & amocrm/oauth2-amocrm

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
-<?php echo "Hello world";
+<?php
+
+$ini = parse_ini_file('../api.ini');
+
+$provider = new \AmoCRM\OAuth2\Client\Provider\AmoCRM([
+    'clientId' => $ini['api_clientId'],
+    'clientSecret' => $ini['api_clientSecret'],
+    'redirectUri' => $ini['api_redirectUri'],
+]);
+
+if (isset($_GET['code']) && $_GET['code']) {
+    //Вызов функции setBaseDomain требуется для установки контектс аккаунта.
+    if (isset($_GET['referer'])) {
+        $provider->setBaseDomain($_GET['referer']);
+    }
+
+    $token = $provider->getAccessToken('authorization_code', [
+        'code' => $_GET['code']
+    ]);
+
+    //todo сохраняем access, refresh токены и привязку к аккаунту и возможно пользователю
+
+    /** @var \AmoCRM\OAuth2\Client\Provider\AmoCRMResourceOwner $ownerDetails */
+    $ownerDetails = $provider->getResourceOwner($token);
+
+    printf('Hello, %s!', $ownerDetails->getName());
+}
