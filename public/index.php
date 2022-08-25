a5529d0993c8c448bfa1c3b6a57bb0eff86ddc45
ryanborisov <ryanborisov@team.amocrm.com>
2022-08-25 11:29:17 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -43,8 +43,11 @@ if(!isset($_GET['code'])){
 		'code' => $_GET['code'],
 	]);
 
-	echo '<script>window.location.href="https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&request=action";</script>';
+	echo '<script>window.location.href="https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&request=true";</script>';
 	exit;
 }
 
-$accessToken = $provider->getToken();
+echo '<br><a href="/task1.php">Задание 1</a> - Добавить в аккаунт 1000 сделок 1000 контактов и 1000 компаний через API, так что бы у каждой сделки была привязана компания и контакт.';
+echo '<br><a href="/task2.php">Задание 2</a> - Добавить кастомное поле типа мультисписок в контакты через API.';
+echo '<br><a href="/task3.php">Задание 3</a> - Обновить все контакты задав значение из этого мультисписка через API.';
+echo '<br><a href="/task4.php">Задание 4</a> - Сделать выгрузку всех сделок контактов и компаний через API и вывести на экран, так что бы вместо id пользователей и кастомных полей отображались реальные названия полей и имена пользователей.';
